3de1d58d9057cb103cecf78961b5cb4c8fb7db98
Stephanie <stephaniepui@outlook.com>
2015-10-08 07:29:42 -0700

    removed auth on api

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,6 +2,7 @@ class IncomingController < ApplicationController
 
   # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
   skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user!
 
   def create
 
